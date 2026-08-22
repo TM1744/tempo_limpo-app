8f3408e6586652c70bda7f9d9ec55e3101dbeec8
TM1744 <marquesthiago1744@gmail.com>
2026-08-22 07:22:37 -0300

    fix/agora a lista de dias retorna apenas o dia atual ou dias futuros

--- src/components/DayList.tsx
+++ src/components/DayList.tsx
@@ -2,9 +2,11 @@ import { StyleSheet, View } from "react-native";
 import { IDayListProps } from "../interfaces/props/IDayListProps";
 import { Notification } from "./Notification";
 import { DayCard } from "./DayCard";
+import { LocalDate } from "@js-joda/core";
 
 export function DayList({ loading, error, days, onPress }: IDayListProps) {
     const hasDays = days !== undefined && days.length > 0;
+    const now = LocalDate.now();
 
     if (error && !hasDays && !loading)
         return <Notification text={error} />
@@ -15,12 +17,15 @@ export function DayList({ loading, error, days, onPress }: IDayListProps) {
     if (hasDays)
         return (
             <View style={style.listContainer}>
-                {days.map((day) => (
-                    <DayCard
-                        day={day}
-                        key={day.id.toString()}
-                        onPress={() => onPress(day)} />
-                ))}
+                {days.map((day) => {
+                    if (!day.date.isBefore(now))
+                        return (
+                            <DayCard
+                                day={day}
+                                key={day.id.toString()}
+                                onPress={() => onPress(day)} />
+                        );
+                })}
             </View>
         );
 
